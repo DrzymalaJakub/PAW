8dfb64b821c0a812da929f5076162942571d603d
JD <jakub.drzymala@uczen.zsk.poznan.pl>
2023-09-25 12:59:58 +0200

    CL(Hello World)

--- 2023.09.25/index.js
+++ 2023.09.25/index.js
@@ -2,7 +2,9 @@ const http = require("http");
 const port = 3300;
 
 const server = http.createServer(function(req, res){
+    res.writeHead(200/*OK*/, {'Content-Type': 'text/html'});
     res.write("Hello World!");
+    console.log("Hello World!")
     res.end();
 });
 
