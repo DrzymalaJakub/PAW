335b426763398252b67e964b74bd1adf00189163
JD <jakub.drzymala@uczen.zsk.poznan.pl>
2023-10-02 14:52:36 +0200

    up to Creating File

--- 2023.10.02/index.js
+++ 2023.10.02/index.js
@@ -1,3 +1,12 @@
+const Json1 = {
+    TeegardenA: "M-class red dwarf",
+    TeegardenAb: "Earth size terrestial",
+    TeegardenAc: "Earth size terrestial",
+};
+const NotFound404 = {
+    Page: "Not Found 404",
+}
+
 const http = require("http");
 const fs = require('fs'); 
 const url = require('url');
@@ -6,21 +15,21 @@ const port = 3300;
 const server = http.createServer(function(req, res){
     let reqUrl = url.parse(req.url, true);
     let fileName = (reqUrl.pathname).replace("/", "");
-    console.log(`URL Recieved: ${fileName}`)
     console.log(`URL: ${reqUrl.pathname}`)
     if(fileName == ""){
         fileName = "index.html";
     }
-    console.log(`File Recieved: ${fileName}`)
+    if(fileName == "api.html"){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(Json1));
+        return;
+    }
     
     fs.readFile(fileName, (err, data)=>{
         if (err){
-            
-            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.writeHead(404, {'Content-Type': 'application/json'});
             console.log(`File not found. Recieved: '${fileName}'`)
-            return res.end("404 Not Found");
-            
-            
+            return res.end(JSON.stringify(NotFound404));
         }
         res.writeHead(200/*OK*/, {'Content-Type': 'text/html'});
         res.write(data);
