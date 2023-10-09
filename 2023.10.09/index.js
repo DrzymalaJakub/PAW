5bbc29d42b627107094b3a773fac3610789e6ffa
JD <jakub.drzymala@uczen.zsk.poznan.pl>
2023-10-09 14:38:55 +0200

    Finished

--- 2023.10.09/index.js
+++ 2023.10.09/index.js
@@ -1,35 +1,49 @@
 const http = require("http");
 const fs = require('fs'); 
 const stream = require('stream');
-//import { Readable } from 'stream
+const util = require("util")
 const port = 3300;
 const minmaxArray = new Array(-420, 2137);
+const finished = util.promisify(stream.finished)
 
-const server = http.createServer(function(req, res){
-    
-    async function * generate(){
-        for(let i = 0; i < 20; i++){
-            yield Math.floor(Math.random() * (minmaxArray[1] - minmaxArray[0]) ) + minmaxArray[0];
-        }
-        
+async function * generate(){
+    for(let i = 0; i < 20; i++){
+        yield Math.floor(Math.random() * (minmaxArray[1] - minmaxArray[0]) ) + minmaxArray[0];
     }
+
+}
+async function writeIterableToFile(iterable, filePath) {
+    const writable = fs.createWriteStream(filePath, {encoding: 'utf8'})
+    for await (const chunk of iterable) {
+        if (!writable.write(chunk)) { // (B)
+            // Handle backpressure
+            await once(writable, 'drain')
+        }
+    }
+    writable.end() // (C)
+    // Wait until done. Throws if there are errors.
+    await finished(writable)
+}
+
+const server = http.createServer(async function(req, res){
+    
     //const wText =  ;
     
     const genNum = stream.Readable.from(generate());
-    const writeStream = fs.createWriteStream(`message-${Date.now().toString()}.txt`, {encoding: 'utf8'});
-    //let message = "";
+    //const writeStream = fs.createWriteStream(`message-${Date.now().toString()}.txt`, {encoding: 'utf8'});
+    let message = new Array();
     genNum.on('data', (chunk) => {
-        console.log(chunk);
-        writeStream.write(chunk.toString());
+        //console.log(chunk.toString());
+        message.push(chunk.toString() + "\n");
     });
     
-    //fs.writeFile(`message-${Date.now().toString()}.txt`, message)
+    genNum.on('end', async()=>{
+        await writeIterableToFile(message, `messages/message-${Date.now().toString()}.txt`);
+        res.writeHead(200/*OK*/, {'Content-Type': 'text/html'});
+        res.write(":)");
+        res.end(); 
+    });
     
-    writeStream.end();
-    
-    res.writeHead(200/*OK*/, {'Content-Type': 'text/html'});
-    res.write(":)");
-    res.end(); 
 });
 
 server.listen(port);
