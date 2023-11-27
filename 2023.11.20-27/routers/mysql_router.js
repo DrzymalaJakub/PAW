e24403ab0b5285dbbb0f87196b59a98cbc648051
Daltochan <jakub.drzymala@uczen.zsk.poznan.pl>
2023-11-27 14:05:24 +0100

    Czy bóg ukrywa się w niebie bo boi się tego co stworzył? btw debuguj fora

--- 2023.11.20-27/routers/mysql_router.js
+++ 2023.11.20-27/routers/mysql_router.js
@@ -44,6 +44,56 @@ router.post(`/connection`, (req, res)=>{
         })
     })
 
+    router.post(`/insert`, (req, res)=>{
+        function GetData(_reqData){ //making input usable
+            console.log("Start of Spliting")
+            class Parameters{
+                constructor(name, nameArgs, query) {
+                    this.name = name;
+                    this.args = nameArgs;
+                    this.params = query;
+                }
+            }
+            console.log(_reqData)
+            console.log(_reqData.nameArgs)
+            const args = `${_reqData.nameArgs}`.split(`{${_reqData.breakWord}}`)
+            const params = `${_reqData.rawQuery}`.split(`{${_reqData.breakWord}}`)
+            console.log(params)
+            return new Parameters(_reqData.name, args, params)
+        }
+
+        const dbConnection = require(`./mysql_router`)
+        dbConnection.connect((err)=>{
+            if(err) throw err
+            console.log("Connected to database for INSERT")
+            const reqData = GetData(req.body);
+            //console.log(dbConnection)
+
+            const _params = (p)=>{
+                let _x = []
+                for(let i = 0; i < p.length; i++){
+                    if( p[i] === ""){
+                        _x.push(`'${p[i]}'`)
+                    }
+                    else{
+                        _x.push(p[i])
+                    }
+                }
+                return _x
+            }
+
+            dbConnection.query(`INSERT INTO ${reqData.name}(${reqData.args.toString()}) VALUES(${_params(reqData.params)})`, (err, result)=>{
+                if(err) throw err
+                console.log(`Success - INSERT`)
+                dbConnection.end((err)=>{
+                    if(err) throw err
+                    console.log("Disconnected from database for INSERT")
+                    res.redirect("/")
+                })
+            })
+        })
+    })
+
 
 
 
