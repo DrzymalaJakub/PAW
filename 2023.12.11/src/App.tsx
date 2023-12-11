2975f851c89eba39852dc615529794ba4118b548
JS <jakub.drzymala@uczen.zsk.poznan.pl>
2023-12-11 13:32:55 +0100

    Navbar 0

--- 2023.12.11/src/App.tsx
+++ 2023.12.11/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-
+        <Navbar />
     </>
   );
 }
---
+++ 2023.12.11/src/components/Navbar/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./index.scss"
+
+function Navbar() :React.JSX.Element{
+    return(
+        <>
+            TODO
+        </>
+    )
+}
+
+export default Navbar
